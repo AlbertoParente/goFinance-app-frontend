f52875771648b4d6cf808e189ca3bb28713fdf9e
albertoparente <albertoparentefh@gmail.com>
2023-03-28 23:41:40 -0300

    Adding and removing

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -17,3 +17,16 @@ export const Label = styled.label`
         color: ${theme.colors.black};
     `}
 `
+export const InputWrapper = styled.label`
+    ${({ theme }) => css`
+        font-family: ${theme.font.family};
+        font-style: normal;
+        font-weidht: ${theme.font.bold}
+        font-size: 16px;
+        line-height: 25px;
+
+        display: flex;
+        align-items: center;
+        color: ${theme.colors.black};
+    `}
+`
