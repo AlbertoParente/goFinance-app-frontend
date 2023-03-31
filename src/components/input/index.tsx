0f89338b936f01fb341026b7e14110958785eed5
albertoparente <albertoparentefh@gmail.com>
2023-03-30 22:40:08 -0300

    Adding +++-

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -3,14 +3,15 @@ import * as S from './styles'
 
 export type InputProps = {
     label?: string
+    placeholder?: string
 } & InputHTMLAttributes<HTMLInputElement>
 
-export default function Input({ name, label }: InputProps) {
+export default function Input({ name, label, placeholder }: InputProps) {
   return (
     <S.Wrapper>
       <S.Label htmlFor={name}>{label}</S.Label>
       <S.InputWrapper>
-        <S.Input />
+        <S.Input placeholder={placeholder} />
       </S.InputWrapper>
     </S.Wrapper>
   )
--- src/components/pages/Login/index.tsx
+++ src/components/pages/Login/index.tsx
@@ -4,7 +4,8 @@ import * as S from './styles'
 export default function Login() {
   return (
     <S.Container>
-      <Input name='login' label='login' />
+      <Input name='login' label='login' placeholder='Enter your name.' />
+      <Input name='password' label='Password' placeholder='Enter your password.' />
     </S.Container>
   )
 }
