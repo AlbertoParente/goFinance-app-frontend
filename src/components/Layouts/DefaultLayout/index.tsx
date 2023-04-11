3e40b30c4be08bcfe5bc1db7610150a923937776
albertoparente <albertoparentefh@gmail.com>
2023-04-10 22:50:09 -0300

    Adding +++

---
+++ src/components/Layouts/DefaultLayout/index.tsx
@@ -0,0 +1,18 @@
+import * as S from './styles'
+
+type DefaultLayoutProps = {
+    children: React.ReactNode
+}
+
+const DefaultLayout = ({ children }: DefaultLayoutProps) => {
+    return (
+        <S.Container>
+            <S.Header>
+                
+            </S.Header>
+            {children}
+        </S.Container>    
+    )
+}
+
+export default DefaultLayout
