3de96c1a6c8c8a9f5247da26c00e92a01e3d22c9
albertoparente <albertoparentefh@gmail.com>
2023-04-17 23:42:41 -0300

    Adding ++

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,33 @@
-import DefaultLayout from '@/components/Layouts/DefaultLayout'
-import Main from '@/components/Main'
+import Chart from "../components/Chart";
+import Card from "../components/Card";
 
-export default function Home() {
-  return (
-    <DefaultLayout>
-      <Main />
-    </DefaultLayout>
-  )
-}
+import { Container, RightSide } from "./styles";
+
+const cardData = [
+  {
+    title: "Saldo a pagar",
+    value: "R$ 130,00",
+  },
+  {
+    title: "Saldo a receber",
+    value: "R$ 200,63",
+  },
+  {
+    title: "Saldo total",
+    value: "R$ 350,13",
+  },
+];
+
+const Home = () => (
+  <Container>
+    <Chart />
+    <RightSide>
+      {cardData.map((card) => (
+        <Card key={card.title} title={card.title} value={card.value} />
+      ))}
+      <button>Download</button>
+    </RightSide>
+  </Container>
+);
+
+export default Home;
